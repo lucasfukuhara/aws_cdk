01e081bf1219498ba1364e6d3b438301d8f4c169
Lucas Fukuhara <62750796+lucasfukuhara@users.noreply.github.com>
2023-02-06 12:19:03 -0300

    Criando Service

--- src/main/java/com/myorg/CursoAwsCdkApp.java
+++ src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -7,9 +7,12 @@ public class CursoAwsCdkApp {
         App app = new App();
 
         VpcStack vpc = new VpcStack(app, "Vpc");
+
         ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpc.getVpc());
         clusterStack.addDependency(vpc); // como o cluster depende da criação da VPC, precisamos setar isso no cdk
 
+        Service01Stack service01Stack = new Service01Stack(app, "Service01", clusterStack.getCluster());
+        service01Stack.addDependency(clusterStack);
 
         app.synth();
     }
